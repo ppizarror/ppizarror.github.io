ee8af69488504e091053b4e91c01dad328eea4b7
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2016-09-19 02:11:59 -0300

    Arreglado mensaje enviado link

--- js/sent-email.js
+++ js/sent-email.js
@@ -1,3 +1,7 @@
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
+
 function message_sent() {
     $(".message-show-after-send").attr('style', 'display:block');
     $(".button-close-after-send").attr('style', 'display:none');
@@ -9,4 +13,12 @@ function message_sent() {
     $(".contactSubject").attr('style', 'background:#222');
     $(".contactMessage").attr('disabled', 'disabled');
     $(".contactMessage").attr('style', 'background:#222; resize: none;');
+}
+
+function check_if_msg_sent() {
+    var vl = getURLParameter("msg_sent");
+    if (vl==1){
+        message_sent();
+        console.log('Establecido en modo mensaje enviado.');
+    }
 }
